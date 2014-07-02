2951253e1cdc00c7bc812926a8b9508ffccad937
elliot <spirityy109@gmail.com>
2014-07-02 23:39:46 +0800

    update pager

--- routes/channel.js
+++ routes/channel.js
@@ -9,10 +9,23 @@ var Reply = mongoose.model('Reply');
 var Channel = mongoose.model('Channel');
 
 //pager num
-var pager_num = 4;
-//var cursor = {};
+var pager_num = 5;
 
-function RenderPage(req,res,topics,channel,pager,callback){
+function BuildPager(cur,total){
+        var pager = {};
+        if(total<=pager_num) return null;
+        var pagertotalnum = Math.ceil(total/pager_num);
+        pager.cur = parseInt(cur);
+        pager.prev = parseInt(cur)-1===0?null:parseInt(cur)-1;
+        pager.next = parseInt(cur)+1>pagertotalnum?null:parseInt(cur)+1;
+        pager.pagenums=[];
+        for(var i=0;i<pagertotalnum;i++){
+            pager.pagenums.push({num:i+1});
+        }
+        return pager;
+}
+
+function RenderListPage(req,res,topics,channel,pager,callback){
         var r_topics = [];
         async.eachSeries(topics,function(topic,cb){
             User.findOne({_id:topic.author_id},function(err,user){
@@ -25,9 +38,6 @@ function RenderPage(req,res,topics,channel,pager,callback){
                 cb();
             });
         },function(err){
-            //cursor.prev=pager.cur;
-            //cursor.first= topics[0];
-            //cursor.last= topics[topics.length-1];
             res.render('list', {
                 topics:r_topics,
                 channel:channel,
@@ -52,8 +62,21 @@ router.post('/add', function(req, res){
 });
 
 router.get('/:url', function(req, res) {
-    res.redirect('/channel/'+req.params.url+'/1');
+    Channel.findOne({url:req.params.url},function(err,channel){
+        var s_option={sort:{create_date:-1}};
+        if(req.query.p){
+            s_option.limit = pager_num;
+            s_option.skip = (req.query.p-1)*pager_num;
+        }else if(channel.topic_count>pager_num){
+            s_option.limit = pager_num;
+            s_option.skip = 0;
+        } 
+        Topic.find({channel_id:channel._id},null,s_option,function(err,topics){
+            RenderListPage(req,res,topics,channel,BuildPager(req.query.p?req.query.p:1,channel.topic_count));
+        });
+    });
 });
+
 
 router.get('/:url/new', function(req, res) {
     res.render('topic/new',{
@@ -76,44 +99,6 @@ router.post('/:url/new', function(req, res) {
             }
         }); 
     })
-});
-
-router.get('/:url/:page', function(req, res) {
-    var skipnum =(req.params.page-1)*pager_num;
-    Channel.findOne({url:req.params.url},function(err,channel){
-
-        //build pager
-        var pager = {};
-        var pagertotalnum = Math.ceil(channel.topic_count/pager_num);
-        pager.cur = parseInt(req.params.page);
-        pager.prev = parseInt(req.params.page)-1===0?null:parseInt(req.params.page)-1;
-        pager.next = parseInt(req.params.page)+1>pagertotalnum?null:parseInt(req.params.page)+1;
-        pager.pagenums=[];
-        for(var i=0;i<pagertotalnum;i++){
-            pager.pagenums.push({num:i+1});
-        }
-
-        //next prev skip
-        /*
-        if(parseInt(req.params.page)-parseInt(cursor.prev) === 1 && cursor.last){
-                Topic.find({channel_id:channel._id,create_date:{$lt:cursor.last.create_date}},null,{limit:pager_num,sort:{create_date:-1}},function(err,topics){
-                    RenderPage(req,res,topics,channel,pager);
-                });
-        }else if(parseInt(req.params.page)-parseInt(cursor.prev) === -1 && cursor.first){
-                Topic.find({channel_id:channel._id,create_date:{$gt:cursor.first.create_date}},null,{limit:pager_num,sort:{create_date:-1}},function(err,topics){
-                    console.info(topics);
-                    RenderPage(req,res,topics,channel,pager);
-                });
-        }else{
-                Topic.find({channel_id:channel._id},null,{limit:pager_num,skip:skipnum,sort:{create_date:-1}},function(err,topics){
-                    RenderPage(req,res,topics,channel,pager);
-                });
-        }
-        */
-        Topic.find({channel_id:channel._id},null,{limit:pager_num,skip:skipnum,sort:{create_date:-1}},function(err,topics){
-            RenderPage(req,res,topics,channel,pager);
-        });
-    });
 });
 
 module.exports = router;
