35e04599d3d5d0c3943ae8ac7d4faafb3be2ae76
elliot <spirityy109@gmail.com>
2014-09-05 18:30:28 +0800

    unfinish uplaod img

--- routes/car.js
+++ routes/car.js
@@ -15,9 +15,68 @@ var fs = require( 'fs' );
 var moment =  require('moment');
 
 router.get('/add',function(req,res){
+    if(!req.session.user){
+        res.redirect('/login');
+    }
     res.render('car/add',{
         title:'添加汽车'
     });
+});
+
+router.post('/add', function(req, res) {
+    var query = {
+        user:req.session.user._id,
+        isown:req.body.isown === 'true'?true:false,
+        brand:req.body.carbrand,
+        name:req.body.carname,
+        desc:req.body.cardesc
+    }
+    if(req.body.engine) query.engine = parseFloat(req.body.engine);
+    if(req.body.turbo) query.turbo = req.body.turbo==='true'?true:false;
+    if(req.body.boxtype) query.boxtype = req.body.boxtype;
+    if(req.body.drivetype) query.drivetype = req.body.drivetype;
+    if(req.body.speed) query.speed = parseFloat(req.body.speed);
+    if(req.body.maxps) query.maxps = parseFloat(req.body.maxps);
+    if(req.body.maxum) query.maxum = parseFloat(req.body.maxum);
+    if(req.body.wheelsize) query.wheelsize = parseFloat(req.body.wheelsize);
+    if(req.body.caritem) query.items  = req.body.caritem;
+
+    new Car(query).save(function(err,car){
+        if(!car || err){
+            res.json({r:0})
+        }else{
+            res.json({r:1,car:car})
+        }
+    });
+});
+
+router.post('/uploadimg', function(req, res) {
+    var form = new formidable.IncomingForm();
+    files = [];
+
+    form.on('file',function(field,file){
+        files.push(file);
+    });
+
+    form.parse(req,function(err){
+        /*
+        var target_floder = process.cwd()+'/public/assets/carimg/'+req.session.user._id;
+        if(!fs.existsSync(target_floder)){
+            if(!fs.existsSync(process.cwd()+'/public/assets/carimg')){
+                fs.mkdirSync(process.cwd()+'/public/assets/caimg');
+            }
+            fs.mkdirSync(target_floder);
+        }
+        */
+        async.eachSeries(files,function(file,cb){
+            console.info(file);
+            cb();
+        },function(){
+            res.json({r:1});
+        });
+
+    });
+
 });
 
 router.get('/:id',function(req,res){
@@ -27,53 +86,10 @@ router.get('/:id',function(req,res){
             return;
         }
         res.render('car/index',{
-            title:car.type+'('+car.brand+')',
+            title:car.name+'('+car.brand+')',
             car:car
         });
     })
-});
-
-router.post('/add', function(req, res) {
-
-        console.info(req.body);
-        var query = {
-            user:req.session.user._id,
-            isown:req.body.isown === 'true'?true:false,
-            brand:req.body.carbrand,
-            name:req.body.carname,
-            desc:req.body.cardesc
-        }
-
-        if(req.body.engine) query.engine = parseFloat(req.body.engine);
-        if(req.body.turbo) query.turbo = req.body.turbo==='true'?true:false;
-        if(req.body.boxtype) query.boxtype = req.body.boxtype;
-        if(req.body.drivetype) query.drivetype = req.body.drivetype;
-        if(req.body.speed) query.speed = parseFloat(req.body.speed);
-        if(req.body.maxps) query.maxps = parseFloat(req.body.maxps);
-        if(req.body.maxum) query.maxum = parseFloat(req.body.maxum);
-        if(req.body.wheelsize) query.wheelsize = parseFloat(req.body.wheelsize);
-        if(req.body.caritem) query.items  = req.body.caritem;
-
-        new Car(query).save(function(err,car){
-            if(!car || err){
-                res.json({r:0})
-            }else{
-                res.json({r:1,car:car})
-            }
-        });
-});
-
-router.post('/uploadimg', function(req, res) {
-    var form = new formidable.IncomingForm(),
-    files = [];
-
-    form.on('file',function(field,file){
-        files.push(file);
-    });
-
-    form.parse(req,function(err){
-        console.info(files);
-    });
 });
 
 module.exports = router;
